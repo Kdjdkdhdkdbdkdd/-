6f4bd0ded3fbaf1c6487825d5fb127021565314b
Kdjdkdhdkdbdkdd <144175234+Kdjdkdhdkdbdkdd@users.noreply.github.com>
2023-09-07 16:13:42 +0300

    Update 3.php

--- 3.php
+++ 3.php
@@ -33,7 +33,7 @@ foreach($users as $user){
     }catch (\danog\MadelineProto\Exception | \danog\MadelineProto\RPCErrorException $e) {
                 try{$MadelineProto->account->updateUsername(['username'=>$user]);
                     $videoLink = 'https://telegra.ph/file/af31cfdaa06f90868a714.mp4';
-                    $caption="ᴛᴇᴀᴍ ꜱᴘᴇᴇᴅ ꫝ 🔱 ! \n - - - - - - - - - - \n ᴜsᴇʀ ⚚ -> @$user \n ᴄʟɪᴄᴋѕ𐇵 -> ❲ $x ❳ \n 𝚂𝙰𝚅𝙴🝡 -> ❲ ᵀᵁᴿᴮᴼ ᶠᴸᴼᴼᴰ ᴮᴼᵀ𖥂 ❳ ⚠️\n - - - - - - - - - - \n 𝒌𝒊𝒏𝒈𝒔🜎 🔰 -> @Kwkkkkkw - @Kwkkkkkw 🏅";
+                    $caption="ᴛᴇᴀᴍ ꜱᴘᴇᴇᴅ ꫝ 🔱 ! \n - - - - - - - - - - \n ᴜsᴇʀ ⚚ -> @$user \n ᴄʟɪᴄᴋѕ𐇵 -> ❲ $x ❳ \n 𝚂𝙰𝚅𝙴🝡 -> ❲ ᵀᵁᴿᴮᴼ ᶠᴸᴼᴼᴰ ᴮᴼᵀ𖥂 ❳ ⚠️\n - - - - - - - - - - \n 𝒌𝒊𝒏𝒈𝒔🜎 🔰 -> @Y_Y_a - @Turbo_ismax 🏅";
                     bot('sendVideo', ['chat_id' => file_get_contents("ID"), 'video' => $videoLink, 'caption' => $caption,]);
                     $data = str_replace("\n".$user,"", file_get_contents("u3"));
                     file_put_contents("u3", $data);
